e27166c5df7cdaf7e7a1281349082523a67f6c5d
AdityaKhapare13 <adityakhapare1234@gmail.com>
2024-12-14 20:21:41 +0530

    update

---
+++ Add.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+class Add{
+    public static int a = 0;
+    public static void main(String[] args){
+        add();
+        add();
+    
+    }
+
+    public static void add(){
+        a = a + 1;
+        System.out.println(a);
+    }
+}
