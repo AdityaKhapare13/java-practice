7678a746245aefa08bc1b863f3bf15c87d4d597e
AdityaKhapare13 <adityakhapare1234@gmail.com>
2024-12-18 20:18:24 +0530

    Update

--- Addition.java
+++ Addition.java
@@ -1,4 +1,4 @@
-import java.awt.*;
+import java.util.*;
 class Addition{
     public static void main(String[] args){
         int a = 12;
--- Substraction.java
+++ Substraction.java
@@ -1,4 +1,4 @@
-import java.awt.*;
+import java.util.*;
 class Substraction{
     public static void main(String[] args){
         int a = 56;
